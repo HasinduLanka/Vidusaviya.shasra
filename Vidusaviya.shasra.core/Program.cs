02928fd32c4e1eade24a97bd625e3c5ad3c9daca
Hasindu <hlrathnayaka@gmail.com>
2020-06-01 12:47:15 +0530

    Add project files.

---
+++ Vidusaviya.shasra.core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Vidusaviya.shasra.core
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Console.Write("hi");
+        }
+    }
+}
