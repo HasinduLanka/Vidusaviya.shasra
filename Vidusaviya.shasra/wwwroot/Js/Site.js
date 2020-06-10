a43887d4c6d1c3db968090c92ad8368f87e4dfc8
harindu wijesinghe <hariwijesinghe234@gmail.com>
2020-06-10 17:56:23 +0530

    added disqus

--- Vidusaviya.shasra/wwwroot/Js/Site.js
+++ Vidusaviya.shasra/wwwroot/Js/Site.js
@@ -1,38 +1,69 @@
-﻿        function cancelFullScreen(el) {
-            var requestMethod = el.cancelFullScreen||el.webkitCancelFullScreen||el.mozCancelFullScreen||el.exitFullscreen;
-            if (requestMethod) { // cancel full screen.
-                requestMethod.call(el);
-            } else if (typeof window.ActiveXObject !== "undefined") { // Older IE.
-                var wscript = new ActiveXObject("WScript.Shell");
-                if (wscript !== null) {
-                    wscript.SendKeys("{F11}");
-                }
-            }
+﻿function cancelFullScreen(el) {
+    var requestMethod = el.cancelFullScreen || el.webkitCancelFullScreen || el.mozCancelFullScreen || el.exitFullscreen;
+    if (requestMethod) { // cancel full screen.
+        requestMethod.call(el);
+    } else if (typeof window.ActiveXObject !== "undefined") { // Older IE.
+        var wscript = new ActiveXObject("WScript.Shell");
+        if (wscript !== null) {
+            wscript.SendKeys("{F11}");
         }
+    }
+}
 
-        function requestFullScreen(el) {
-            // Supports most browsers and their versions.
-            var requestMethod = el.requestFullScreen || el.webkitRequestFullScreen || el.mozRequestFullScreen || el.msRequestFullscreen;
+function requestFullScreen(el) {
+    // Supports most browsers and their versions.
+    var requestMethod = el.requestFullScreen || el.webkitRequestFullScreen || el.mozRequestFullScreen || el.msRequestFullscreen;
 
-            if (requestMethod) { // Native full screen.
-                requestMethod.call(el);
-            } else if (typeof window.ActiveXObject !== "undefined") { // Older IE.
-                var wscript = new ActiveXObject("WScript.Shell");
-                if (wscript !== null) {
-                    wscript.SendKeys("{F11}");
-                }
-            }
-            return false
+    if (requestMethod) { // Native full screen.
+        requestMethod.call(el);
+    } else if (typeof window.ActiveXObject !== "undefined") { // Older IE.
+        var wscript = new ActiveXObject("WScript.Shell");
+        if (wscript !== null) {
+            wscript.SendKeys("{F11}");
         }
+    }
+    return false
+}
 
-        function toggleFull() {
-            var elem = document.body; // Make the body go full screen.
-            var isInFullScreen = (document.fullScreenElement && document.fullScreenElement !== null) ||  (document.mozFullScreen || document.webkitIsFullScreen);
+function toggleFull() {
+    var elem = document.body; // Make the body go full screen.
+    var isInFullScreen = (document.fullScreenElement && document.fullScreenElement !== null) || (document.mozFullScreen || document.webkitIsFullScreen);
 
-            if (isInFullScreen) {
-                cancelFullScreen(document);
-            } else {
-                requestFullScreen(elem);
-            }
-            return false;
-        }
+    if (isInFullScreen) {
+        cancelFullScreen(document);
+    } else {
+        requestFullScreen(elem);
+    }
+    return false;
+}
+
+function openChat() {
+    document.getElementById("chatBox").style.display = "block";
+}
+
+function closeChat() {
+    document.getElementById("chatBox").style.display = "none";
+}
+
+function togleChat() {
+    var isopen = document.getElementById("chatBox").style.display == "block";
+    if (isopen) {
+        closeChat();
+    }
+    else {
+        openChat();
+        initChatBox();
+    }
+}
+
+var disqus_config = function () {
+    this.page.url = "https://localhost:5001/meet";  // Replace PAGE_URL with your page's canonical URL variable
+    this.page.identifier = 98134989189842949194199489;
+};
+function initChatBox() {
+    var d = document.getElementById("chatBox"), s = document.createElement('script');
+    s.src = 'https://vidusaviya-shasthra.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    d.appendChild(s);
+
+}
